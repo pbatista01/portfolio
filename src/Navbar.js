0d40192f159b92c4f93531fb0280f035f6e3fbf1
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-02-27 21:06:39 -0500

    Adding css animation to the menu button

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import './navbar.css';
 function Navbar() {
 
     const openMenu = () =>{
-      alert("Button clicked");
+      
     }
 
     return (
@@ -13,10 +13,19 @@ function Navbar() {
           <Logo />
           <h1>Estefanie</h1>
         </div>
-        <div className="navbar-btn" onClick={openMenu}>
+        <div className="navbar-btn menu-open" onClick={openMenu}>
           <span></span>
           <span></span>
           <span></span>
+        </div>
+        <div className="navbar-menu menu-closed">
+          <span>X</span>
+          <ul>
+            <li><a src="#">Home</a></li>
+            <li><a src="#">Projects</a></li>
+            <li><a src="#">About Me</a></li>
+            <li><a src="#">Contact Me</a></li>
+          </ul>
         </div>
       </div>
     );
